0772c178b711a853a1b641f95d12f4db7d23df67
fabiopassos21 <fabiopassos21>
2022-08-24 14:14:24 -0300

    Tela Login completa

--- Login/app/src/main/java/com/projeto/login/FormLogin.java
+++ Login/app/src/main/java/com/projeto/login/FormLogin.java
@@ -1,16 +1,33 @@
 package com.projeto.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.text.Normalizer;
 
 public class FormLogin extends AppCompatActivity {
 private TextView text_tela_cadastro;
+private EditText edit_emaill,edit_senha;
+private Button bt_entrar;
+private ProgressBar progressbar;
+    private String [] mensagens={"Preencha todos os campos","Login realizado com sucesso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +35,91 @@ private TextView text_tela_cadastro;
         setContentView(R.layout.activity_form_login);
         getSupportActionBar().hide();
         iniciarComponentes();
-            text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(FormLogin.this,FormCadastro.class);
                     startActivity(intent);
                 }
             });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_emaill.getText().toString();
+                String senha = edit_senha.getText().toString();
+                if (email.isEmpty()||senha.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(v,mensagens[0], Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }else {
+                    AuthenticarUsuario(v);
+                }
+            }
+        });
+    }
+// manter login
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+        if (usuarioAtual!=null) {
+            TelaPrincipal();
+
+        }
+
+    }
+
+    public void AuthenticarUsuario(View v) {
+        String email = edit_emaill.getText().toString();
+        String senha = edit_senha.getText().toString();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressbar.setVisibility(View.VISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+            TelaPrincipal();
+                        }
+                    },3000);
+            }else {
+                    String erro;
+                try {
+                    throw task.getException();
+                }catch (Exception e){
+                    erro = "Erro ao logar";
+                }
+                Snackbar snackbar = Snackbar.make(v,erro, Snackbar.LENGTH_LONG);
+                snackbar.setBackgroundTint(Color.WHITE);
+                snackbar.setTextColor(Color.BLACK);
+                snackbar.show();
+        }
+            }
+    });
+
+    }
+
+
+
+    private void TelaPrincipal () {
+        Intent intent = new Intent( FormLogin.this, TelaPrincipal.class);
+startActivity(intent);
+finish();
     }
     public void iniciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+            edit_emaill = findViewById(R.id.text_email);
+            edit_senha = findViewById(R.id.text_senha);
+            bt_entrar = findViewById(R.id.bt_entrar);
+            progressbar = findViewById(R.id.progressbar);
+
     }
 }
